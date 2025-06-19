be2daf48e1dfd2a93bf3b800a55b1fe58de0ff68
gui2310g <fontesguilherme57@gmail.com>
2025-06-18 23:22:54 -0300

    [feat] added sign up dialog in login form page

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,21 +1,22 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { ReusableDialog } from './ui/dialog'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 export function LoginForm({
   className,
   ...props
-}: React.ComponentProps<"div">) {
+}: React.ComponentProps<'div'>) {
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
         <CardHeader>
           <CardTitle>Login to your account</CardTitle>
@@ -51,16 +52,47 @@ export function LoginForm({
                 <Button type="submit" className="w-full cursor-pointer">
                   Login
                 </Button>
-                <Button variant="outline" className="w-full cursor-pointer">
-                  Login with Google
-                </Button>
               </div>
             </div>
             <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
+              Don&apos;t have an account?{' '}
+              <ReusableDialog
+                trigger={
+                  <a href="#" className="underline underline-offset-4">
+                    Sign up
+                  </a>
+                }
+                title="Sign up"
+                description="Preencha os campos para criar sua conta."
+                onConfirm={() => {
+                  console.log('Usuario adicionado!')
+                }}
+              >
+                <form>
+                  <label htmlFor="nome">Nome:</label>
+                  <input
+                    type="text"
+                    placeholder="Nome"
+                    id="nome"
+                    className="w-full border rounded p-2 mb-2"
+                    required
+                  />
+                  <label htmlFor="nome">Email:</label>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full border rounded p-2 mb-2"
+                    required
+                  />
+                  <label htmlFor="nome">Senha:</label>
+                  <input
+                    type="password"
+                    placeholder="Senha"
+                    className="w-full border rounded p-2 mb-2"
+                    required
+                  />
+                </form>
+              </ReusableDialog>
             </div>
           </form>
         </CardContent>
